6a2ac64ad6f6c80aa97ad99b77b451edd4dc5cdf
AnhVu910 <conanshinichi2000@gmail.com>
2023-10-13 23:16:02 +0700

    done UI signin signup

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,9 +1,115 @@
-import React from 'react'
+import { Image } from "antd";
+import React, { useState } from "react";
+import InputForm from "../../components/InputForm/InputForm";
+import { WrapperButton } from "../../components/ProductDetailComponent/style";
+import {
+  WrapperContainerLeft,
+  WrapperContainerRight,
+  WrapperTextLight,
+} from "./style";
+import imageLogo from "../../assets/images/logo-login.png";
+import { EyeFilled, EyeInvisibleFilled } from "@ant-design/icons";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 const SignInPage = () => {
-  return (
-    <div>SignInPage</div>
-  )
-}
+  const [isShowPassword, setIsShowPassword] = useState(false);
+  const location = useLocation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
 
-export default SignInPage
+  const navigate = useNavigate();
+
+  const handleNavigateSignUp = () => {
+    navigate("/sign-up");
+  };
+
+  const handleOnchangeEmail = (value) => {
+    setEmail(value);
+  };
+
+  const handleOnchangePassword = (value) => {
+    setPassword(value);
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          width: "800px",
+          height: "445px",
+          borderRadius: "6px",
+          background: "#fff",
+          display: "flex",
+        }}
+      >
+        <WrapperContainerLeft>
+          <h1>Xin chào !</h1>
+          <p>Đăng nhập vào tạo tài khoản</p>
+          <InputForm style={{ marginBottom: "20px" }} placeholder="Email" />
+          <div style={{ position: "relative" }}>
+            <span
+              onClick={() => setIsShowPassword(!isShowPassword)}
+              style={{
+                zIndex: 10,
+                position: "absolute",
+                top: "4px",
+                right: "8px",
+              }}
+            >
+              {isShowPassword ? <EyeFilled /> : <EyeInvisibleFilled />}
+            </span>
+            <InputForm
+              placeholder="Mật khẩu"
+              type={isShowPassword ? "text" : "password"}
+              value={password}
+              onChange={handleOnchangePassword}
+            />
+          </div>
+          <WrapperButton
+            style={{
+              fontSize: "15px",
+              fontWeight: "700",
+              background: "rgb(255, 57, 69)",
+              color: "#fff",
+              width: "100%",
+              margin: "26px 0 10px",
+            }}
+            disabled={!email.length || !password.length}
+            // onClick={handleSignIn}
+          >
+            Đăng nhập
+          </WrapperButton>
+          <p>
+            <WrapperTextLight>Quên mật khẩu?</WrapperTextLight>
+          </p>
+          <p>
+            Chưa có tài khoản?
+            <WrapperTextLight onClick={handleNavigateSignUp}> Tạo tài khoản</WrapperTextLight>
+          </p>
+        </WrapperContainerLeft>
+        <WrapperContainerRight>
+          <Image
+            src={imageLogo}
+            preview={false}
+            alt="iamge-logo"
+            height="203px"
+            width="203px"
+          />
+          <h4>Mua sắm tại Chợ 2hand</h4>
+        </WrapperContainerRight>
+      </div>
+    </div>
+  );
+};
+
+export default SignInPage;
--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,9 +1,135 @@
-import React from 'react'
+import { Image } from "antd";
+import React, { useState } from "react";
+import InputForm from "../../components/InputForm/InputForm";
+import { WrapperButton } from "../../components/ProductDetailComponent/style";
+import {
+  WrapperContainerLeft,
+  WrapperContainerRight,
+  WrapperTextLight,
+} from "./style";
+import imageLogo from "../../assets/images/logo-login.png";
+import { useNavigate } from "react-router-dom";
+import { EyeFilled, EyeInvisibleFilled } from "@ant-design/icons";
 
 const SignUpPage = () => {
-  return (
-    <div>SignUpPage</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default SignUpPage
+  const [isShowPassword, setIsShowPassword] = useState(false);
+  const [isShowConfirmPassword, setIsShowConfirmPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleOnchangeEmail = (value) => {
+    setEmail(value);
+  };
+  const handleOnchangePassword = (value) => {
+    setPassword(value);
+  };
+
+  const handleOnchangeConfirmPassword = (value) => {
+    setConfirmPassword(value);
+  };
+
+  const handleNavigateSignIn = () => {
+    navigate("/sign-in");
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          width: "800px",
+          height: "445px",
+          borderRadius: "6px",
+          background: "#fff",
+          display: "flex",
+        }}
+      >
+        <WrapperContainerLeft>
+          <h1>Xin chào !</h1>
+          <p>Đăng ký tài khoản</p>
+          <InputForm style={{ marginBottom: "15px" }} placeholder="Email" />
+          <div style={{ position: "relative" }}>
+            <span
+              onClick={() => setIsShowPassword(!isShowPassword)}
+              style={{
+                zIndex: 10,
+                position: "absolute",
+                top: "4px",
+                right: "8px",
+              }}
+            >
+              {isShowPassword ? <EyeFilled /> : <EyeInvisibleFilled />}
+            </span>
+            <InputForm
+              placeholder="Mật khẩu"
+              style={{ marginBottom: "15px" }}
+              type={isShowPassword ? "text" : "password"}
+              value={password}
+              onChange={handleOnchangePassword}
+            />
+          </div>
+          <div style={{ position: "relative" }}>
+            <span
+              onClick={() => setIsShowConfirmPassword(!isShowConfirmPassword)}
+              style={{
+                zIndex: 10,
+                position: "absolute",
+                top: "4px",
+                right: "8px",
+              }}
+            >
+              {isShowConfirmPassword ? <EyeFilled /> : <EyeInvisibleFilled />}
+            </span>
+            <InputForm
+              placeholder="Nhập lại mật khẩu"
+              type={isShowConfirmPassword ? "text" : "password"}
+              value={confirmPassword}
+              onChange={handleOnchangeConfirmPassword}
+            />
+          </div>
+          <WrapperButton
+            style={{
+              fontSize: "15px",
+              fontWeight: "700",
+              background: "rgb(255, 57, 69)",
+              color: "#fff",
+              width: "100%",
+              margin: "26px 0 10px",
+            }}
+            // onClick={handleAddOrderProduct}
+          >
+            Đăng ký
+          </WrapperButton>
+
+          <p>
+            Đã có tài khoản?
+            <WrapperTextLight onClick={handleNavigateSignIn}>
+              {" "}
+              Đăng nhập ngay
+            </WrapperTextLight>
+          </p>
+        </WrapperContainerLeft>
+        <WrapperContainerRight>
+          <Image
+            src={imageLogo}
+            preview={false}
+            alt="iamge-logo"
+            height="203px"
+            width="203px"
+          />
+          <h4>Mua sắm tại Chợ 2hand</h4>
+        </WrapperContainerRight>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
